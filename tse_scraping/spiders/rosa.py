a87aef0ada1d249613826f2396b878fd093031fd
Heitorado <hscs@cin.ufpe.br>
2019-03-30 12:49:00 -0300

    Tweak some of Rosa's methods and add new scraping fields, following the same structure

--- tse_scraping/spiders/rosa.py
+++ tse_scraping/spiders/rosa.py
@@ -15,7 +15,8 @@ class RosaSpider(scrapy.Spider):
         "uf": r'(\b[A-z]{2}\b)',
         "protocol_number": r'(^[0-9]+)',
         "date": r'(\d{1,2}\/\d{1,2}\/\d{4})',
-        "time": r'(\d{1,2}:\d{1,2})'
+        "time": r'(\d{1,2}:\d{1,2})',
+        "anything": r'(.+)'
     }
     
     inc = 1
@@ -24,6 +25,12 @@ class RosaSpider(scrapy.Spider):
     municipio = ""
     uf = ""
     protocolo = ""
+    representantes = "" 
+    representados = ""
+    relator = "" 
+    assunto = "" 
+    localizacao = "" 
+    fase_atual = "" 
 
     def parse(self, response):
         for processo in response.xpath("//table"):
@@ -46,10 +53,20 @@ class RosaSpider(scrapy.Spider):
                     'processo_num': self.numproc, #processo.xpath("//tr/td[1]/b/text()").get(),
                     'municipio': self.municipio,
                     'uf': self.uf,
-                    'protocol': {
-                        'number': extract_literal_regex_only(self.protocolo, self.regexDict["protocol_number"]),
-                        'date': extract_literal_regex_only(self.protocolo, self.regexDict["date"]),
-                        'time': extract_literal_regex_only(self.protocolo, self.regexDict["time"])
+                    'protocolo': {
+                        'numero': extract_literal_regex_only(self.protocolo, self.regexDict["protocol_number"]),
+                        'data': extract_literal_regex_only(self.protocolo, self.regexDict["date"]),
+                        'hora': extract_literal_regex_only(self.protocolo, self.regexDict["time"])
+                    },
+                    'representantes': self.representantes,
+                    'representados': self.representados,
+                    'relator': self.relator,
+                    'assunto': extract_literal_regex_only(self.assunto, self.regexDict["anything"]),
+                    'localizacao': extract_literal_regex_only(self.localizacao, self.regexDict["anything"]),
+                    'fase_atual': {
+                        'data_ultima_atualizacao': extract_literal_regex_only(self.fase_atual, self.regexDict["date"]),
+                        'hora_ultima_atualizacao': extract_literal_regex_only(self.fase_atual, self.regexDict["time"]),
+                        'comentario': extract_literal_regex_only(self.fase_atual, self.regexDict["anything"]),
                     }
                 }
             except TypeError as t:
@@ -64,10 +81,16 @@ class RosaSpider(scrapy.Spider):
             yield scrapy.Request(response.urljoin(next_page_url))
 
 def reset_attributes(self):
-    self.numproc = ""
+    self.numproc = None
     self.municipio = ""
     self.uf = ""
     self.protocolo = ""
+    self.representantes = "" 
+    self.representados = ""
+    self.relator = "" 
+    self.assunto = "" 
+    self.localizacao = "" 
+    self.fase_atual = "" 
 
 def get_corresponding_attribute(self, selector, current_element):
     if("PROCESSO" in selector):
@@ -86,6 +109,36 @@ def get_corresponding_attribute(self, selector, current_element):
         self.protocolo = current_element.xpath(".//td/text()").getall()
         self.protocolo = remove_special_characters_all(self.protocolo)
         self.protocolo = remove_empty_strings(self.protocolo)
+    elif("REPRESENTANT" in selector):
+        self.representantes = current_element.xpath(".//td/text()").getall()
+        self.representantes = unidecode_all(self.representantes)
+        self.representantes = remove_special_characters_all(self.representantes)
+        self.representantes = remove_empty_strings(self.representantes)
+    elif("REPRESENTAD" in selector):
+        self.representados = current_element.xpath(".//td/text()").getall()
+        self.representados = unidecode_all(self.representados)
+        self.representados = remove_special_characters_all(self.representados)
+        self.representados = remove_empty_strings(self.representados)
+    elif("RELATOR" in selector):
+        self.relator = current_element.xpath(".//td/text()").getall()
+        self.relator = unidecode_all(self.relator)
+        self.relator = remove_special_characters_all(self.relator)
+        self.relator = remove_empty_strings(self.relator)
+    elif("ASSUNTO" in selector):
+        self.assunto = current_element.xpath(".//td/text()").getall()
+        self.assunto = unidecode_all(self.assunto)
+        self.assunto = remove_special_characters_all(self.assunto)
+        self.assunto = remove_empty_strings(self.assunto)
+    elif("LOCALIZAC" in selector):
+        self.localizacao = current_element.xpath(".//td/text()").getall()
+        self.localizacao = unidecode_all(self.localizacao)
+        self.localizacao = remove_special_characters_all(self.localizacao)
+        self.localizacao = remove_empty_strings(self.localizacao)
+    elif("FASE" in selector):
+        self.fase_atual = current_element.xpath(".//td/text()").getall()
+        self.fase_atual = unidecode_all(self.fase_atual)
+        self.fase_atual = remove_special_characters_all(self.fase_atual)
+        self.fase_atual = remove_empty_strings(self.fase_atual)
 
 
 
@@ -109,7 +162,7 @@ def remove_empty_strings(arr):
 def remove_special_characters_all(str_arr):
     clean_arr = []
     for word in str_arr:
-        clean_arr.append(word.replace("\n","").replace("\t","").strip())
+        clean_arr.append(word.replace("\n","").replace("\t","").replace("\r","").strip())
     
     return clean_arr
 
