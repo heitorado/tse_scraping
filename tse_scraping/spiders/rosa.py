a90db457eff8f8d1b627fc8695fc8723706bf137
Heitorado <hscs@cin.ufpe.br>
2019-03-30 21:45:49 -0300

    Add new attributes and scraping directives for all the possible titles that can be found on the "Partes" section

--- tse_scraping/spiders/rosa.py
+++ tse_scraping/spiders/rosa.py
@@ -23,6 +23,7 @@ class RosaSpider(scrapy.Spider):
     test_prot_codes = [100011996,100011997,100012016,10002011,10002012,10002016,100022015,100032007,100032016,100042006,100042016,100062003,100062008,100062009,100072004,100082004,100082015,100102003,100102012,100112011,10012011,100121998,100122005,100151996,100152001,100162010,100162016,100172004,100172014,100172016,100182003,100191998,1002002,100202013,10021960,10022010,10022016,100222006,100222011,100222016,100232011,100232016,100242011,100242016,100251997,100252009,100252011,100261992,100262003,100262011,100272007,100281998,100282008,100292001,100292016,100301996,100302003,100302004,100312002,100312003,10031990,10031998,10032000,10032017,100321995,100322003,100332001,100332003,100332006,100342000,100342001,100342007,100351998,100352001,100352012,100361996,100362006,100371996,100371997,100402003,100402005,10042000,10042007,10042017,100422003,100422005,100432006,100441997,100462004,100472014,100482005,100492003,100492011,100492012,100502003,100502011,10052016,100532011,100542008,100551989,100551998,100561997,100562004,100562012,100571997,100582004,100602004,100602014,100612004,10062003,10062011,10062017,100622005,100622007,100622013,100631998,100632004,100632015,100652004,100652013,100661990,100662009,100681998,100682004,100682016,100691998,100692004,100692008,100692012,100692016,100702009,100702012,100702015,100712013,10071994,10072007,100722006,100722012,100732002,100732016,100741998,100742002,100742006,100742014,100751998,100761998,100762006,100771997,100771998,100772009,100792004,100802004,100811996,10082016,100832001,100832006,100832008,100842004,100842006,100842011,100842016,100851995,100852004,100872006,100892007,100892015,100902012,100902014,100911989,100912001,100912008,100912014,100912016,10091998,10092006,10092016,100922008,100922011,100922014,100922016,100932016,100941998,100942004,100942016,100952001,100952016,100962014,100962016,100972006,100982002,100982013,100992007,10102006,10102017,101022004,101022012,101032004,101042002,101042005,101042009,101042012,101042013,101042015,101051998,101052004,101052013,101061994,101061995,101062004,101062006,101072006,101072014,101072015,101081998,101082008,101082015,101091997,101091998,101092012,101092015,101102006,101112016,10112016,101122010,101122016,101131995,101132010,101142001,101142011,101152003,101152004,101162002,101162003,101172002,101172009,101172014,101181997,101182014,101182015,1011999,101212003,10122006,10122009,10122013,101222008,101232015,101242008,101242009,101252001,101271998,101282001,101292006,101302003,101302009,101312009,101312010,101312011,10132016,10132017,101322004,101331996,101332001,101332003,101332004,101332016,101341996,101341998,101342000,101342004,101351996,101351998,101362004,101372013,101372016,101381996,101382001,101382015,101382016,101391996,101392016,101401997,101402005,101402012,101402016,101412000,101412011,101412016,10141999,10142003,10142006,10142017,101422000,101422006,101431995,101432008,101442000,101452005,101452015,101472000,101481990,101482000,101482005,101482007,101492000,101492012,101501996,101502008,101512005,101512013,10152003,10152006,101531996,101532003,101532008,101541998,101542004,101542006,101542008,101551998,101562006,101562011,101572000,101572003,101572006,101581996,101582008,101591992,101591998,101592008,101592010,101601998,101602000,101602004,101602014,101611998,101612000,101612004,10161998,10162003,10162014,10162017,101622000,101622004,101622005,101622010,101622012,101631994,101632004,101642000,101642004,101642009,101642016,101651998,101652000,101652004,101652012,101681997,101702000,101702003,101702004,101712013,10171998,10172003,10172018,101722003,101722011,101731998,101732004,101732012,101732016,101742016,101751998,101752012,101761998,101762012,101762016,101772006,101781998,101782000,101791997,101791998,101792006,101801997,101801998,101802012,101811992,101811998,101812005,101812006,101812013,10182006,10182007,101821990,101821998,101832003,101832004,101842005,101852005,101862010,101862016]    
     inc = 1
 
+    # Metadados do processo
     identificacao = ""
     numproc = ""
     municipio = ""
@@ -33,7 +34,40 @@ class RosaSpider(scrapy.Spider):
     relator = "" 
     assunto = "" 
     localizacao = "" 
-    fase_atual = "" 
+    fase_atual = ""
+    
+
+    # Possíveis nomes das Partes do Processo
+    apelantes = []
+    apelados = []
+    agravantes = []
+    agravados = []
+    recorrentes = []
+    recorridos = []
+    embargantes = []
+    embargados = []
+    impetrantes = []
+    impetrados = []
+    requerentes = []
+    requeridos = []
+    reclamantes = []
+    reclamados = []
+    exequentes = []
+    executados = []
+    demandantes = []
+    demandados = []
+    denunciantes = []
+    denunciados = []
+    excipientes = []
+    exceptos = []
+    querelantes = []
+    querelados = []
+    autores = []
+    reus = []
+
+    # Generalização das partes, separando em polo ativo ou passivo
+    polo_ativo = []
+    polo_passivo = []
 
     def parse(self, response):
         for processo in response.xpath("//table"):
@@ -57,19 +91,58 @@ class RosaSpider(scrapy.Spider):
                     'processo_num': extract_literal_regex_only(self.numproc, self.regexDict["process_number"]), #processo.xpath("//tr/td[1]/b/text()").get(),
                     'processo_vinculado_num': "todo",
                     'municipio': extract_literal_regex_only(self.municipio, self.regexDict["municipio"]),
-                    'uf': self.uf,#extract_literal_regex_only(self.municipio, self.regexDict["uf"]),
+                    'uf': self.uf,
                     'protocolo': {
                         'numero': extract_literal_regex_only(self.protocolo, self.regexDict["protocol_number"]),
                         'data': extract_literal_regex_only(self.protocolo, self.regexDict["date"]),
                         'hora': extract_literal_regex_only(self.protocolo, self.regexDict["time"])
                     },
                     'partes': {
-                        'representantes': join_and_split_by_comma(self.representantes),
-                        'representados': join_and_split_by_comma(self.representados),
-                        # adicionar coisas
+                        'representantes': self.representantes,
+                        'representados': self.representados,
+
+                        'apelantes': self.apelantes,
+                        'apelados': self.apelados,
+
+                        'agravantes': self.agravantes,
+                        'agravados': self.agravados,
+
+                        'recorrentes': self.recorrentes,
+                        'recorridos': self.recorridos,
+
+                        'embargantes': self.embargantes,
+                        'embargados': self.embargados,
+
+                        'impetrantes': self.impetrantes,
+                        'impetrados': self.impetrados,
+
+                        'requerentes': self.requerentes,
+                        'requeridos': self.requeridos,
+
+                        'reclamantes': self.reclamantes,
+                        'reclamados': self.reclamados,
+
+                        'exequentes': self.exequentes,
+                        'executados': self.executados,
+
+                        'demandantes': self.demandantes,
+                        'demandados': self.demandados,
+
+                        'denunciantes': self.denunciantes,
+                        'denunciados': self.denunciados,
+
+                        'excipientes': self.excipientes,
+                        'exceptos': self.exceptos,
+
+                        'querelantes': self.querelantes,
+                        'querelados': self.querelados,
+
+                        'autores': self.autores,
+                        'reus': self.reus,
+
                         'polos': {
-                            'polo_ativo': "todo",
-                            'polo_passivo': "todo"
+                            'polo_ativo': self.polo_ativo,
+                            'polo_passivo': self.polo_passivo
                         }
                     },
                     'relator': extract_matching_string_from_list(self.relator, self.regexDict["anything"]),
@@ -94,19 +167,53 @@ class RosaSpider(scrapy.Spider):
             yield scrapy.Request(response.urljoin(next_page_url))
 
 def reset_attributes(self):
+    # Metadados do processo
     self.identificacao = ""
     self.numproc = ""
     self.municipio = ""
     self.uf = ""
     self.protocolo = ""
-    self.representantes = "" 
-    self.representados = ""
     self.relator = "" 
     self.assunto = "" 
     self.localizacao = "" 
-    self.fase_atual = "" 
+    self.fase_atual = ""
+
+    # Possíveis nomes das partes do processo
+    self.representantes = [] 
+    self.representados = []
+    self.apelantes = []
+    self.apelados = []
+    self.agravantes = []
+    self.agravados = []
+    self.recorrentes = []
+    self.recorridos = []
+    self.embargantes = []
+    self.embargados = []
+    self.impetrantes = []
+    self.impetrados = []
+    self.requerentes = []
+    self.requeridos = []
+    self.reclamantes = []
+    self.reclamados = []
+    self.exequentes = []
+    self.executados = []
+    self.demandantes = []
+    self.demandados = []
+    self.denunciantes = []
+    self.denunciados = []
+    self.excipientes = []
+    self.exceptos = []
+    self.querelantes = []
+    self.querelados = []
+    self.autores = []
+    self.reus = []
+
+    # Generalização das partes
+    self.polo_ativo = []
+    self.polo_passivo = []
 
 def get_corresponding_attribute(self, selector, current_element):
+    # Identificadores dos metadados do processo
     if("IDENTIFICACAO" in selector):
         self.identificacao = current_element.xpath(".//td/text()").getall()
         self.identificacao = unidecode_all(self.identificacao)
@@ -131,16 +238,6 @@ def get_corresponding_attribute(self, selector, current_element):
         self.protocolo = current_element.xpath(".//td/text()").getall()
         self.protocolo = remove_special_characters_all(self.protocolo)
         self.protocolo = remove_empty_strings(self.protocolo)
-    elif("REPRESENTANT" in selector):
-        self.representantes = current_element.xpath(".//td/text()").getall()
-        self.representantes = unidecode_all(self.representantes)
-        self.representantes = remove_special_characters_all(self.representantes)
-        self.representantes = remove_empty_strings(self.representantes)
-    elif("REPRESENTAD" in selector):
-        self.representados = current_element.xpath(".//td/text()").getall()
-        self.representados = unidecode_all(self.representados)
-        self.representados = remove_special_characters_all(self.representados)
-        self.representados = remove_empty_strings(self.representados)
     elif("RELATOR" in selector):
         self.relator = current_element.xpath(".//td/text()").getall()
         self.relator = unidecode_all(self.relator)
@@ -161,7 +258,104 @@ def get_corresponding_attribute(self, selector, current_element):
         self.fase_atual = unidecode_all(self.fase_atual)
         self.fase_atual = remove_special_characters_all(self.fase_atual)
         self.fase_atual = remove_empty_strings(self.fase_atual)
+    # Inicio da verificação das partes do processo
+    elif("REPRESENTANT" in selector):
+        self.representantes.append(get_and_sanitize_string(current_element, self.representantes))
+        self.polo_ativo = self.representantes
+    elif("REPRESENTAD" in selector):
+        self.representados.append(get_and_sanitize_string(current_element, self.representados))
+        self.polo_passivo = self.representados
+    elif ('APELANT' in selector):
+        self.apelantes.append(get_and_sanitize_string(current_element, self.apelantes))
+        self.polo_ativo = self.apelantes
+    elif ('APELAD' in selector): 
+        self.apelados.append(get_and_sanitize_string(current_element, self.apelados))
+        self.polo_passivo = self.apelados
+    elif ('AGRAVANT' in selector):
+        self.agravantes.append(get_and_sanitize_string(current_element, self.agravantes))
+        self.polo_ativo = self.agravantes
+    elif ('AGRAVAD' in selector):
+        self.agravados.append(get_and_sanitize_string(current_element, self.agravados))
+        self.polo_passivo = self.agravados
+    elif ('RECORRENT' in selector):
+        self.recorrentes.append(get_and_sanitize_string(current_element, self.apelantes))
+        self.polo_ativo = self.recorrentes
+    elif ('RECORRID' in selector):
+        self.recorridos.append(get_and_sanitize_string(current_element, self.recorridos))
+        self.polo_passivo = self.recorridos
+    elif ('EMBARGANT' in selector):
+        self.embargantes.append(get_and_sanitize_string(current_element, self.embargantes))
+        self.polo_ativo = self.embargantes
+    elif ('EMBARGAD' in selector):
+        self.embargados.append(get_and_sanitize_string(current_element, self.embargados))
+        self.polo_passivo = self.embargados
+    elif ('IMPETRANT' in selector):
+        self.impetrantes.append(get_and_sanitize_string(current_element, self.impetrantes))
+        self.polo_ativo = self.impetrantes
+    elif ('IMPETRAD' in selector):
+        self.impetrados.append(get_and_sanitize_string(current_element, self.impetrados))
+        self.polo_passivo = self.impetrados
+    elif ('REQUERENT' in selector):
+        self.requerentes.append(get_and_sanitize_string(current_element, self.requerentes))
+        self.polo_ativo = self.requerentes
+    elif ('REQUERID' in selector):
+        self.requeridos.append(get_and_sanitize_string(current_element, self.requeridos))
+        self.polo_passivo = self.requeridos
+    elif ('RECLAMANT' in selector):
+        self.reclamantes.append(get_and_sanitize_string(current_element, self.reclamantes))
+        self.polo_ativo = self.reclamantes
+    elif ('RECLAMAD' in selector):
+        self.reclamados.append(get_and_sanitize_string(current_element, self.reclamados))
+        self.polo_passivo = self.reclamados
+    elif ('EXEQUENT' in selector):
+        self.exequentes.append(get_and_sanitize_string(current_element, self.exequentes))
+        self.polo_ativo = self.exequentes
+    elif ('EXECUTAD' in selector):
+        self.executados.append(get_and_sanitize_string(current_element, self.executados))
+        self.polo_passivo = self.executados
+    elif ('DEMANDANT' in selector):
+        self.demandantes.append(get_and_sanitize_string(current_element, self.demandantes))
+        self.polo_ativo = self.demandantes
+    elif ('DEMANDAD' in selector):
+        self.demandados.append(get_and_sanitize_string(current_element, self.demandados))
+        self.polo_passivo = self.demandados
+    elif ('DENUNCIANT' in selector):
+        self.denunciantes.append(get_and_sanitize_string(current_element, self.denunciantes))
+        self.polo_ativo = self.denunciantes
+    elif ('DENUNCIAD' in selector):
+        self.denunciados.append(get_and_sanitize_string(current_element, self.denunciados))
+        self.polo_passivo = self.denunciados
+    elif ('EXCIPIENT' in selector):
+        self.excipientes.append(get_and_sanitize_string(current_element, self.excipientes))
+        self.polo_ativo = self.excipientes
+    elif ('EXCEPT' in selector):
+        self.exceptos.append(get_and_sanitize_string(current_element, self.exceptos))
+        self.polo_passivo = self.exceptos
+    elif ('QUERELANT' in selector):
+        self.querelantes.append(get_and_sanitize_string(current_element, self.querelantes))
+        self.polo_ativo = self.querelantes
+    elif ('QUERELAD' in selector):
+        self.querelados.append(get_and_sanitize_string(current_element, self.querelados))
+        self.polo_passivo = self.querelados
+    elif ('AUTOR' in selector):
+        self.autores.append(get_and_sanitize_string(current_element, self.autores))
+        self.polo_ativo = self.autores
+    elif ('RE' in selector):
+        self.reus.append(get_and_sanitize_string(current_element, self.reus))
+        self.polo_passivo = self.reus
+    
 
+# Gets all the strings from the current "element" passed as parameter using the xpath(".//td/text()") and:
+# - Unidecode everything
+# - Remove special characters line \n \r \t
+# - Remove all empty strings ("")
+# The resulting string will be stored in the variable passed in the parameter 'var'
+def get_and_sanitize_string(element, var):
+    var = element.xpath(".//td/text()").getall()
+    var = unidecode_all(var)
+    var = remove_special_characters_all(var)
+    var = remove_empty_strings(var)
+    return var
 
 
 def parse_text(text):
